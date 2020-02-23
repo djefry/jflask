9df68a10f2efab394cd6048addab763db3122952
wandashare <wandashare@localhost.localdomain>
2020-02-23 22:28:01 +0700

    Add automate sending email with celery

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from app.database import init_engine, init_db
-
-# Initialize db without configuring
-#db = SQLAlchemy()
+from celery import Celery
 
 
 def create_app(config=None):
@@ -18,6 +16,15 @@ def create_app(config=None):
     init_db()
     app.config['SECRET_KEY'] = 'secret'
 
+    # Mail configuration
+    app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+    app.config['MAIL_PORT'] = 2525
+    app.config['MAIL_MAX_EMAILS'] = 3
+    app.config['MAIL_USERNAME'] = '4ef0a76264776f'
+    app.config['MAIL_PASSWORD'] = '9772f11bff889a'
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
+
     # Initialize any extension and bind blueprint
     from .main.views import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -25,3 +32,29 @@ def create_app(config=None):
     app.register_blueprint(event_blueprint)
 
     return app
+
+
+def make_celery(app):
+    # Celery configuration
+    app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+    app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+    app.config['CELERYBEAT_SCHEDULE'] = {
+        # Executes every minute
+        'execute-every-60-seconds': {
+            'task': 'periodic_task',
+            'schedule': 30.0,
+        }
+    }
+
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
--- run.py
+++ run.py
@@ -1,5 +1,68 @@
-from app import create_app
+import datetime
+from http.client import HTTPException
+from app import create_app, make_celery
+from sqlalchemy import and_
+from celery.utils.log import get_task_logger
+from app.event.models import EventModel, MemberModel
+from app.database import db_session
+from flask_mail import Mail, Message
 
+# App initialization
 app = create_app()
+Session = db_session()
+
+# Email initialization
+mail = Mail(app)
+
+# Celery initialization
+logger = get_task_logger(__name__)
+celery = make_celery(app)
+
+# Async Task
+@celery.task(name="async_task", auto_retry=[HTTPException], max_retries=5)
+def send_email(data):
+    # with app.app_context():
+    with mail.connect() as conn:
+        msg = Message(data['subject'],
+                      sender="admin@jflask.com",
+                      recipients=[data['email']])
+        msg.body = data['content']
+        # conn.send(msg)
+        logger.info("Async task complete")
+
+
+# Periodic task, beat speed in setting/config
+@celery.task(name="periodic_task")
+def periodic_task():
+    # Get time for UTC+8
+    time_now = (datetime.datetime.utcnow()+datetime.timedelta(hours=8))
+    # Select timestamp where email sent status false
+    qs_date = Session.query(EventModel).filter(EventModel.sent == True)
+    for date in qs_date:
+        # Compare the timestamp with time now
+        if date.timestamp <= time_now:
+            # Joint EventModel and Member Model to get email address
+            qs_mail = Session.query(EventModel).filter(and_(EventModel.event_id == date.event_id),
+                                                       (EventModel.members)).one()
+            print("Hasil Query :{}".format(qs_mail.sent))
+            email_subject = qs_mail.email_subject.decode("utf-8")
+            email_content = qs_mail.email_content.decode("utf-8")
+            # Get email address
+            data = {}
+            for mail in qs_mail.members:
+                data['email'] = (mail.email)
+            logger.info("Email: {}".format(data['email']))
+            data['subject'] = email_subject
+            data['content'] = email_content
+            # Send async email
+            send_email.delay(data)
+            # Change sent status to true
+            qs_mail.sent = True
+            Session.commit()
+
+    # print('Time Now : {}'.format(time_now))
+    logger.info("Periodic task run")
+
+
 if __name__ == '__main__':
-    app.run('127.0.0.1', 5000, debug=True)
+    app.run('0.0.0.0', 8000, debug=True)
