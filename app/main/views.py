d9835628836da14b0191d1d7fa8f5502ed9e1d4f
wandashare <wandashare@localhost.localdomain>
2020-02-23 21:29:57 +0700

    showing the data to main view

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,17 @@
 from flask import current_app, Blueprint, render_template
+from app.database import db_session
+from app.event.models import EventModel, MemberModel
 
 main = Blueprint('main', __name__)
+Session = db_session()
 
 
 @main.route("/")
 def index():
-    return render_template('home.html', title='Welcome to JFlask')
+    app = current_app._get_current_object()
+    obj = Session.query(EventModel).filter(
+        EventModel.members).order_by(EventModel.event_id.desc()).all()
+    context = {
+        'obj': obj,
+    }
+    return render_template('home.html', title='Welcome to JFlask', context=context)
