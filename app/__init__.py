e7dbc0e3e727a3540845d0b3155d5a45186d9908
wandashare <wandashare@localhost.localdomain>
2020-02-24 18:37:55 +0700

    Add setdate and move configuration to setting

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,18 @@
 from flask import Flask
+import datetime
 from app.database import init_engine, init_db
 from celery import Celery
 
 
 def create_app(config=None):
+    # App Initializtion
     app = Flask(__name__)
+    app.config.from_object(config)
 
-    if config is not None:
-        app.config.from_object(config)
-
-    # Database configuration, releated to database.py
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    # Database Initialization
     init_engine(app.config['SQLALCHEMY_DATABASE_URI'],
                 encoding='utf8')
     init_db()
-    app.config['SECRET_KEY'] = 'secret'
-
-    # Mail configuration
-    app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
-    app.config['MAIL_PORT'] = 2525
-    app.config['MAIL_MAX_EMAILS'] = 3
-    app.config['MAIL_USERNAME'] = '4ef0a76264776f'
-    app.config['MAIL_PASSWORD'] = '9772f11bff889a'
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
 
     # Initialize any extension and bind blueprint
     from .main.views import main as main_blueprint
@@ -35,17 +24,7 @@ def create_app(config=None):
 
 
 def make_celery(app):
-    # Celery configuration
-    app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-    app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-    app.config['CELERYBEAT_SCHEDULE'] = {
-        # Executes every minute
-        'execute-every-60-seconds': {
-            'task': 'periodic_task',
-            'schedule': 30.0,
-        }
-    }
-
+    # Celery initialization
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
@@ -58,3 +37,8 @@ def make_celery(app):
                 return TaskBase.__call__(self, *args, **kwargs)
     celery.Task = ContextTask
     return celery
+
+
+# Set date to UTC+8
+def setdate():
+    return (datetime.datetime.utcnow()+datetime.timedelta(hours=8))
---
+++ settings.py
@@ -0,0 +1,27 @@
+# App configuration
+class ConfigApp:
+    # Database Configuration
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+
+    # Secret Key
+    SECRET_KEY = 'secret'
+
+    # Mail configuration
+    MAIL_SERVER = 'smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_MAX_EMAILS = 1
+    MAIL_USERNAME = '4ef0a76264776f'
+    MAIL_PASSWORD = '9772f11bff889a'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
+    # Celery configuration
+    CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERYBEAT_SCHEDULE = {
+        # Executes every minute
+        'execute-every-60-seconds': {
+            'task': 'periodic_task',
+            'schedule': 60.0,
+        }
+    }
