836d2782a3bf8a780c38442413db2e9f4dc55bcf
wandashare <wandashare@localhost.localdomain>
2020-02-24 23:16:09 +0700

    Add custom validation

--- app/event/form.py
+++ app/event/form.py
@@ -1,19 +1,24 @@
-import datetime
-from sqlalchemy import func
+from app import setdate
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectMultipleField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 from wtforms.fields.html5 import DateTimeField
 from .models import MemberModel, EventModel
+
+
+def date_check(form, field):
+    if field.data < setdate():
+        raise ValidationError("You can't set schedule to past")
 
 
 class FormEvent(FlaskForm):
 
     email_subject = StringField('Subject', validators=[DataRequired()])
     email_content = StringField('Content', validators=[DataRequired()])
-    member = SelectMultipleField('Member', coerce=int)
+    member = SelectMultipleField(
+        'Member', coerce=int, validators=[DataRequired()])
     timestamp = DateTimeField('Date Send', validators=[DataRequired(
-    )], default=datetime.datetime.utcnow()+datetime.timedelta(hours=8), format='%Y-%m-%d %H:%M')
+    ), date_check], default=setdate(), format='%Y-%m-%d %H:%M')
     submit = SubmitField('Save Email')
 
 
