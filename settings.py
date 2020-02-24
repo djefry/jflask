925ade9e4b8657165b60b684ad4a8cb18ebb8d57
wandashare <wandashare@localhost.localdomain>
2020-02-24 23:21:34 +0700

    Change database to postgresql

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
-# App configuration
+
+
+# Object configuration
 class ConfigApp:
     # Database Configuration
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:secret@postgres/postgres'
 
     # Secret Key
     SECRET_KEY = 'secret'
@@ -16,8 +18,8 @@ class ConfigApp:
     MAIL_USE_SSL = False
 
     # Celery configuration
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERY_BROKER_URL = 'redis://redis:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
     CELERYBEAT_SCHEDULE = {
         # Executes every minute
         'execute-every-60-seconds': {
