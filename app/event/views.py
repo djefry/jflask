45f7d5b9cdd4232b225013b1e8fdbb93315192d5
wandashare <wandashare@localhost.localdomain>
2020-02-25 22:55:21 +0700

    Adding try and except

--- app/event/views.py
+++ app/event/views.py
@@ -10,26 +10,34 @@ Session = db_session()
 @event.route('/save_emails', methods=['GET', 'POST'])
 def save_emails():
     form = FormEvent()
-    form.member.choices = [(m.member_id, m.username)
-                           for m in Session.query(MemberModel).all()]
+    try:
+        # Select member_id and username to populate the choice
+        form.member.choices = [(m.member_id, m.username)
+                               for m in Session.query(MemberModel).all()]
+    except Exception as e:
+        return "Database connection failed, Please contact your administrator."
+
     if form.validate_on_submit():
         event_data = EventModel(
             email_subject=form.email_subject.data,
             email_content=form.email_content.data,
             timestamp=form.timestamp.data
         )
-        for member in form.member.data:
-            qs = Session.query(MemberModel).filter(
-                MemberModel.member_id == member).first()
-            event_data.members.append(qs)
-        Session.add(event_data)
         try:
+            # Save the relation between event and members
+            for member in form.member.data:
+                qs = Session.query(MemberModel).filter(
+                    MemberModel.member_id == member).first()
+                event_data.members.append(qs)
+
+            Session.add(event_data)
             Session.commit()
             flash('Email saved with Subject: {}, Content: {}'.format(
                 form.email_subject.data, form.email_content.data))
             return redirect("/")
         except Exception as e:
-            flash("Data saving failed, Please try again")
+            Session.rollback()
+            return "Data saving failed, Please try again"
     return render_template('save_emails.html', title='Save Emails', form=form)
 
 
@@ -43,7 +51,12 @@ def add_member():
             email=form.email.data,
             username=form.username.data
         )
-        Session.add(member_data)
-        Session.commit()
+        try:
+            # Saving data member
+            Session.add(member_data)
+            Session.commit()
+        except Exception as e:
+            Session.rollback()
+            flash("Data saving failed, Please try again")
         return redirect("/")
     return render_template('add_member.html', title='Add Member', form=form)
